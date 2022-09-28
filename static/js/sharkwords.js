f0d2e549b9e36ae47e43ae8b413d65bbe743bbd7
suelenk <suelenmatosr@gmail.com>
2022-09-28 18:38:20 -0400

    done until the disableLetterButton function

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -20,13 +20,21 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+
+  for (let letter of word){
+    let createDivs = document.querySelector('#word-container').insertAdjacentHTML('beforeend', `<div class="letter-box ${letter}"></div>`);
+  }
+  
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  for (let letter of ALPHABET){
+    let generate_button = document.querySelector('#letter-buttons').insertAdjacentHTML('beforeend', `<button> ${letter} </button>`);
+  }
+ 
+
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,7 +42,8 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+ buttonEl.disabled = true
+ 
 };
 
 // This is a helper function we will use in the future
@@ -49,17 +58,18 @@ const isLetterInWord = (letter) => {
 // It will be called when the file is run (because
 // we call the function on line 66)
 (function startGame() {
-  // For now, we'll hardcode the word that the user has to guess
+  // For now, we'll hardcode the word that the user has to guesss
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
   const word = 'hello';
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+
+  createDivsForChars(word)
 
   // call the function that makes a button for each letter in the alphabet
-  // Replace this line with the function call
+  generateLetterButtons(ALPHABET)
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
